e0da220a0fad459dcd2056fa9b2a8577d12688f0
AlejandroC05 <yeineralejandro3@gmail.com>
2024-06-02 16:10:43 -0500

    Cambios y actualizaciones

--- vendor/open-admin-org/open-admin/resources/views/login.blade.php
+++ vendor/open-admin-org/open-admin/resources/views/login.blade.php
@@ -1,302 +1,104 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<title>Iniciar Sesión</title>
-		<link rel="icon" href="{{ asset('img/softige-icon.ico') }}" type="image/x-icon">
-		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <style type="text/tailwindcss">
+    		@layer utilities {
+      			.content-auto {
+        			content-visibility: auto;
+      			}
+    		}
 
-		@if(!is_null($favicon = Admin::favicon()))
-		<link rel="shortcut icon" href="{{$favicon}}">
-		@endif
+        body.bg-color {
+					background-image: url('/images/background.jpg');
+					background-repeat: no-repeat;
+          background-position: center;
+					background-size: cover;
+				}
 
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-		<link rel="stylesheet" href="{{ Admin::asset("open-admin/css/styles.css")}}">
-		<script src="{{ Admin::asset("bootstrap5/bootstrap.bundle.min.js")}}"></script>
+        @keyframes slideInFromLeft {
+          from {
+            transform: translateX(-100%);
+            opacity: 0;
+          }
+          to {
+            transform: translateX(0);
+            opacity: 1;
+          }
+        }
 
-		<style>
-			body.bg-color {
-				background-image: url("{{ asset('img/model.jpg') }}");
-				background-repeat: no-repeat;
-				background-size: cover;
-			}
-		</style>
-	</head>
-	<body class="bg-color" @if(config('admin.login_background_image'))style="background: url({{config('admin.login_background_image')}}) no-repeat;background-size: cover;"@endif>
-		<!-- <div class="d-flex justify-content-center align-items-center h-100">
-			<div class="container m-4" style="max-width:400px;">
-				<h1 class="text-center mb-3 h2 fw-bold"><a class="text-decoration-none text-white" href="{{ admin_url('/') }}">{{config('admin.name')}}</a></h1>
-				<div class="bg-body p-4 shadow-sm rounded-3">
+        @keyframes appear {
+          from {
+            opacity: 0;
+          }
+          to {
+            opacity: 1;
+          }
+        }
 
-					@if($errors->has('attempts'))
-						<div class="alert alert-danger m-0 text-center">{{$errors->first('attempts')}}</div>
-					@else
+        .animate-slideInFromLeft {
+          animation: slideInFromLeft 1s ease-out;
+        }
 
-					<form action="{{ admin_url('auth/login') }}" method="post">
+        .animate-appear {
+          animation: appear 2s ease-out;
+        }
+  </style>
+</head>
+<body class="bg-color" @if(config('admin.login_background_image'))style="background: url({{config('admin.login_background_image')}}) no-repeat;background-size: cover;"@endif>
+  <div class="flex justify-center items-center min-h-screen">
+    <div class="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-md xl:max-w-md bg-gradient-to-r from-blue-900 to-violet-900 rounded-sm shadow-2xl overflow-hidden p-8 space-y-8 animate-slideInFromLeft">
+      <h2 class="text-center text-4xl font-extrabold text-white animate-appear">
+        Welcome
+      </h2>
+      <p class="text-center text-gray-200 animate-appear">
+        Sign in to your account
+      </p>
+      @if($errors->has('attempts'))
+        <div class="alert alert-danger m-0 text-center">{{$errors->first('attempts')}}</div>
+      @else
+      <form method="post" action="{{ admin_url('auth/login') }}" class="space-y-6">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-						<div class="mb-3">
+        <!-- Inicio correo y contraseña -->
+        <div class="relative">
+          <input placeholder="john@example.com" class="peer h-10 w-full border-b-2 border-gray-300 text-white bg-transparent placeholder-transparent focus:outline-none focus:border-purple-500" name="username" id="username" value="{{ old('username') }}" required>
+          <label class="absolute left-0 -top-3.5 text-gray-500 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-purple-500 peer-focus:text-sm" for="username">Usuario</label>
+        </div>
 
-							@if($errors->has('username'))
-								<div class="alert alert-danger">{{$errors->first('username')}}</div>
-							@endif
+        <div class="relative">
+          <input placeholder="Password" class="peer h-10 w-full border-b-2 border-gray-300 text-white bg-transparent placeholder-transparent focus:outline-none focus:border-purple-500" required="" name="password" id="password" type="password" />
+          <label class="absolute left-0 -top-3.5 text-gray-500 text-sm transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-2 peer-focus:-top-3.5 peer-focus:text-purple-500 peer-focus:text-sm" for="password">Contraseña</label>
+          @if($errors->has('password'))
+            <div class="alert alert-danger">{{$errors->first('password') }}</div>
+          @endif
+        </div>
+        <!-- Fin correo y contraseña -->
 
-							<label for="username" class="form-label">{{ __('admin.username') }}</label>
-							<div class="input-group mb-3">
-								<span class="input-group-text"><i class="fa-solid fa-user"></i></span>
-								<input type="text" class="form-control" placeholder="{{ __('admin.username') }}" name="username" id="username" value="{{ old('username') }}" required>
-							</div>
-						</div>
+        @if($errors->has('username'))
+          <div class="alert text-center text-warning">¡Usuario o contraseña incorrectos!</div>
+        @endif
 
-						<div class="mb-3">
-							<label for="password" class="form-label">{{ __('admin.password') }}</label>
-							<div class="input-group mb-3">
-								<span class="input-group-text"><i class="fa-solid fa-key"></i></span>
-								<input type="password" class="form-control" placeholder="{{ __('admin.password') }}" name="password" id="password" required>
-							</div>
+        @if(config('admin.auth.remember'))
+        <div class="flex items-center justify-between">
+          <label class="flex items-center text-sm text-gray-200">
+            <input class="form-checkbox h-4 w-4 text-purple-600 bg-gray-800 border-gray-300 rounded" type="checkbox" name="remember" id="remember" value="1"  {{ (old('remember')) ? 'checked="checked"' : '' }}>
+            <span class="ml-2" for="remember">Recuérdame</span>
+          </label>
+        <a class="text-sm text-purple-200 hover:underline" target="_blank" href="web.whatsapp.com">Forgot your password?</a>
+        </div>
+        @endif
 
-							@if($errors->has('password'))
-								<div class="alert alert-danger">{{$errors->first('password') }}</div>
-							@endif
-						</div>
-
-						@if(config('admin.auth.remember'))
-						<div class="mb-3 form-check">
-							<input type="checkbox" class="form-check-input" name="remember" id="remember" value="1"  {{ (old('remember')) ? 'checked="checked"' : '' }}>
-							<label class="form-check-label" for="remember">{{ __('admin.remember_me') }}</label>
-						</div>
-						@endif
-					
-						<a class="btn btn-secondary float-start" href="/">Regresar</a>
-
-						<div class="clearfix">
-							<button type="submit" class="btn float-end btn-primary">{{ __('Continuar') }}</button>
-						</div>
-
-					</form>
-					@endif
-				</div>
-			</div>
-		</div> -->
-		<div class="d-flex justify-content-center align-items-center h-100">
-		
-			<div class="login-box">
-  				<p>Login</p>
-				  	@if($errors->has('attempts'))
-						<div class="alert alert-danger m-0 text-center">{{$errors->first('attempts')}}</div>
-					@else
-  					<form action="{{ admin_url('auth/login') }}" method="post">
-					  	<input type="hidden" name="_token" value="{{ csrf_token() }}">
-    					<div class="user-box">
-							<label for="username" class="form-label"></label>
-							@if($errors->has('username'))
-								<div class="alert">{{$errors->first('username')}}</div>
-							@endif
-      						<input type="text" class="form-control" name="username" id="username" value="{{ old('username') }}" required>
-      						<label>Email</label>
-    					</div>
-    					<div class="user-box">
-							<label for="password" class="form-label"></label>
-      						<input type="password" class="form-control" name="password" id="password" required>
-      						<label>Password</label>
-							@if($errors->has('password'))
-								<div class="alert alert-danger">{{$errors->first('password') }}</div>
-							@endif
-						</div>
-							
-						<button type="submit">
-							<span></span>
-							<span></span>
-							<span></span>
-							<span></span>
-							Submit
-						</button>
-  					</form>
-					@endif
-  				<p>Don't have an account? <a href="" class="a2">Sign up!</a></p>
-			</div>
-		</div>
-	</body>
-	<style>
-		.login-box {
-  			position: absolute;
-  			top: 50%;
-  			left: 50%;
-  			width: 400px;
-  			padding: 40px;
-  			margin: 20px auto;
-  			transform: translate(-50%, -55%);
-  			background: rgba(0,0,0,.9);
-  			box-sizing: border-box;
-  			box-shadow: 0 15px 25px rgba(0,0,0,.6);
-  			border-radius: 10px;
-		}
-
-.login-box p:first-child {
-  margin: 0 0 30px;
-  padding: 0;
-  color: #fff;
-  text-align: center;
-  font-size: 1.5rem;
-  font-weight: bold;
-  letter-spacing: 1px;
-}
-
-.login-box .user-box {
-  position: relative;
-}
-
-.login-box .user-box input {
-  width: 100%;
-  padding: 10px 0;
-  font-size: 16px;
-  color: #fff;
-  margin-bottom: 30px;
-  border: none;
-  border-bottom: 1px solid #fff;
-  outline: none;
-  background: transparent;
-}
-
-.login-box .user-box label {
-  position: absolute;
-  top: 0;
-  left: 0;
-  padding: 10px 0;
-  font-size: 16px;
-  color: #fff;
-  pointer-events: none;
-  transition: .5s;
-}
-
-.login-box .user-box input:focus ~ label,
-.login-box .user-box input:valid ~ label {
-  top: -20px;
-  left: 0;
-  color: #fff;
-  font-size: 12px;
-}
-
-.login-box form button {
-  position: relative;
-  display: inline-block;
-  padding: 10px 20px;
-  font-weight: bold;
-  color: #fff;
-  font-size: 16px;
-  text-decoration: none;
-  text-transform: uppercase;
-  overflow: hidden;
-  transition: .5s;
-  margin-top: 40px;
-  letter-spacing: 3px
-}
-
-.login-box button:hover {
-  background: #fff;
-  color: #272727;
-  border-radius: 5px;
-}
-
-.login-box button span {
-  position: absolute;
-  display: block;
-}
-
-.login-box button span:nth-child(1) {
-  top: 0;
-  left: -100%;
-  width: 100%;
-  height: 2px;
-  background: linear-gradient(90deg, transparent, #fff);
-  animation: btn-anim1 1.5s linear infinite;
-}
-
-@keyframes btn-anim1 {
-  0% {
-    left: -100%;
-  }
-
-  50%,100% {
-    left: 100%;
-  }
-}
-
-.login-box button span:nth-child(2) {
-  top: -100%;
-  right: 0;
-  width: 2px;
-  height: 100%;
-  background: linear-gradient(180deg, transparent, #fff);
-  animation: btn-anim2 1.5s linear infinite;
-  animation-delay: .375s
-}
-
-@keyframes btn-anim2 {
-  0% {
-    top: -100%;
-  }
-
-  50%,100% {
-    top: 100%;
-  }
-}
-
-.login-box button span:nth-child(3) {
-  bottom: 0;
-  right: -100%;
-  width: 100%;
-  height: 2px;
-  background: linear-gradient(270deg, transparent, #fff);
-  animation: btn-anim3 1.5s linear infinite;
-  animation-delay: .75s
-}
-
-@keyframes btn-anim3 {
-  0% {
-    right: -100%;
-  }
-
-  50%,100% {
-    right: 100%;
-  }
-}
-
-.login-box button span:nth-child(4) {
-  bottom: -100%;
-  left: 0;
-  width: 2px;
-  height: 100%;
-  background: linear-gradient(360deg, transparent, #fff);
-  animation: btn-anim4 1.5s linear infinite;
-  animation-delay: 1.125s
-}
-
-@keyframes btn-anim4 {
-  0% {
-    bottom: -100%;
-  }
-
-  50%,100% {
-    bottom: 100%;
-  }
-}
-
-.login-box p:last-child {
-  color: #aaa;
-  font-size: 14px;
-}
-
-.login-box button.a2 {
-  color: #fff;
-  text-decoration: none;
-}
-
-.login-box button.a2:hover {
-  background: transparent;
-  color: #aaa;
-  border-radius: 5px;
-}
-	</style>
+        <button class="w-full py-2 px-4 bg-purple-500 hover:bg-purple-700 rounded-md shadow-lg text-white font-semibold transition duration-200" type="submit">Ingresar</button>
+      </form>
+      <div class="text-center text-gray-300">
+        Don't have an account? <a class="text-purple-300 hover:underline" href="#">Sign up</a>
+      </div>
+      @endif
+    </div>
+  </div>
+</body>
 </html>
